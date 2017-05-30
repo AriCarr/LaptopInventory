4398f90900e9fbe5245058a60d56dc2cff4779a3
Ari Carr <acarr@brandeis.edu>
2017-05-30 12:40:56 -0400

    Print auth hash to logs

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ApplicationRecord
 
   class << self
     def from_omniauth(auth_hash)
-        puts auth_hash
+        puts "Keys: #{auth_hash.keys}"
+        auth_hash.keys.each do |k|
+          puts "#{k}: #{auth_hash[k]}"
+        end
     #   user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
     #   user.name = auth_hash['info']['name']
     #   user.location = auth_hash['info']['location']
