64a33796b317c6182fe4fd86b15d1f845446ca39
Ari Carr <acarr@brandeis.edu>
2017-05-30 12:54:37 -0400

    Try to find username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ApplicationRecord
 
   class << self
     def from_omniauth(auth_hash)
-        puts "Keys: #{auth_hash['extra'].keys}"
-        auth_hash['extra'].keys.each do |k|
-          puts "#{k}: #{auth_hash[k]}"
+        puts "Keys: #{auth_hash['extra']['raw_info'].keys}"
+        auth_hash['extra']['raw_info'].keys.each do |k|
+          puts "#{k}: #{auth_hash['extra']['raw_info'][k]}"
         end
       user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
       user.name = auth_hash['info']['name']
-      user.username = auth_hash['extra']['mailNickname']
+      user.username = auth_hash['extra']['raw_info']['mailNickname']
       user.save!
       puts user
       user
