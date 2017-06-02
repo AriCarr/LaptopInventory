d4ee5668dccc6d98933802679e8d2eaa2dd767c2
Ari Carr <acarr@brandeis.edu>
2017-06-02 09:21:55 -0400

    Frontend work + DRY

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -5,4 +5,20 @@ class Computer < ApplicationRecord
 
   enum status: [ :in_use, :available, :junk ]
   enum manufacturer: [ :"Apple", :"Dell", :"HP", :"Lenovo", :"Microsoft", :"Other" ]
+
+  def title
+    "#{name} | S/N: #{serial}"
+  end
+
+  def date
+    "#{updated_at.month}/#{updated_at.day}/#{updated_at.year}"
+  end
+
+  def possessive_name
+    "#{owner}'s #{manufacturer} #{model} (#{status.humanize})"
+  end
+
+  def specs
+    "RAM: #{ram} GB | Storage: #{space} GB | Processor: #{processor} GHz"
+  end
 end
