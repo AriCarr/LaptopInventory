94f3549a4b041edbbc1c68c6e50d0d3be56199bc
Ari Carr <acarr@brandeis.edu>
2017-06-22 11:57:00 -0400

    Edited set_computer

--- app/controllers/computers_controller.rb
+++ app/controllers/computers_controller.rb
@@ -108,7 +108,7 @@ class ComputersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_computer
-      @computer = Computer.find(params[:id])
+      @computer = Computer.all.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
