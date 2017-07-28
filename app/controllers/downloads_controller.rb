0ee46cdd7edd0f9ca6b97cd9c661ab665380162e
Ari Carr <acarr@brandeis.edu>
2017-07-28 14:49:00 -0400

    Added 32-bit HWiNFO option

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,7 +1,11 @@
 class DownloadsController < ApplicationController
 
-  def sysinfo
+  def sysinfo_64
     send_file "#{Rails.root}/public/HWiNFO64.exe", filename: "HWiNFO64.exe"
+  end
+
+  def sysinfo_32
+    send_file "#{Rails.root}/public/HWiNFO64.exe", filename: "HWiNFO32.exe"
   end
 
 end
