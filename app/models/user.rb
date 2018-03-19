382f5aa6d3feebfd7b93088694ba7bf9e75fb25d
Ari Carr <acarr@brandeis.edu>
2018-03-19 11:48:46 -0400

    Account for case where user emails start with uppercase letters

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   class << self
     def from_omniauth(auth_hash)
-      u = find_or_create_by(name: auth_hash['info']['name'], email: auth_hash['extra']['raw_info']['mail'])
+      u = find_or_create_by(name: auth_hash['info']['name'], email: auth_hash['extra']['raw_info']['mail'].downcase)
       if u.email == ENV['PRESEED_ADMIN_EMAIL']
         u.is_admin = true
         u.save!
